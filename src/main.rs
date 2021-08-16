928cf79e05292027786e03dcba5730cac5a400d9
JD <upkanet@gmail.com>
2021-08-16 18:09:15 +0200

    Read i16

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+extern crate file_utils;
+
+use std::io;
+use std::fs::File;
+
+use file_utils::read::Read;
+
+fn main() -> io::Result<()>
+{
     println!("RSpiker launch");
+    let mut file = File::open("data/40014.raw")?;
+    let x: i16 = file.read_i16()?;
+    print!("{}\n", x.to_string());
+
+    Ok(())
+}
+
+pub fn bytes_to_i16(x: &[u8; 2]) -> i16
+{
+    ((x[1] as i16) <<  8) +
+    ((x[0] as i16) <<  0)
 }
