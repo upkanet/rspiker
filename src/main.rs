8910015dc5bd9e4e3cbdaf690c0065c2291b6815
JD <upkanet@gmail.com>
2021-08-16 21:46:23 +0200

    EOH finder

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use std::fs::File;
 use std::str;
 use std::io::SeekFrom;
 
-use file_utils::read::Read;
+//use file_utils::read::Read;
 
 fn main() -> io::Result<()>
 {
-    println!("RSpiker launch");
-    let mut file = File::open("data/40014.raw")?;
+    //println!("RSpiker launch");
+    let i = findeof();
+    print!("{}\n", i);
+    /*let mut file = File::open("data/40014.raw")?;
 
     //Header
     let mut buffer = [0; 168];
@@ -35,5 +37,26 @@ fn main() -> io::Result<()>
     let x: i16 = file.read_i16()?;
     print!("x = {}\n", x.to_string());
 
+    let i = findEOF();
+    print!("{}\n", i);*/
+
     Ok(())
+}
+
+fn findeof() -> u64
+{
+    let mut file = File::open("data/40014.raw").expect("Introuvable");
+    let mut buffer = [0; 3];
+    let mut n = 169;
+    loop {
+        io::Seek::seek(&mut file, SeekFrom::Start(n)).expect("No");
+        io::Read::read(&mut file, &mut buffer).expect("Introuvable");
+        let s = str::from_utf8(&buffer).expect("erreur");
+        n+=1;
+        if s == "EOH" {
+            n+=2;
+            break;
+        }
+    }
+    return n;
 }
