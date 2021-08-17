cb292b36bce593664e23b94b624ede5ad89fbe04
JD <upkanet@gmail.com>
2021-08-17 22:45:23 +0200

    Web Server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use std::io;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+use rocket::State;
+
 use std::time::Instant;
 
 mod record;
 use record::Record;
 
-fn main() -> io::Result<()>
+#[get("/electrode/len/<e>")]
+fn hello(r: State<Record>, e: String) -> String {
+    let ne:usize = e.parse().unwrap();
+    return r.electrodes[ne].len().to_string();
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    println!("RSpiker launch");
+    let mut r = Record::new("data/0.raw".to_string());
+    let now = Instant::now();
+    println!("Loading data...");
+    r.load();
+    println!("Loading Data - Time elapsed : {}", now.elapsed().as_secs());
+    rocket::ignite()
+        .manage(r)
+        .mount("/", routes![index,hello])
+        .launch();
+}
+
+
+/*fn main() -> io::Result<()>
 {
     println!("RSpiker launch");
     let mut r = Record::new("data/90003.raw".to_string());
@@ -22,4 +51,4 @@ fn main() -> io::Result<()>
     r.raster(2);
 
     Ok(())
-}
+}*/
