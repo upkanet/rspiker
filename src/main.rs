194c5b7827e9d5debc0424cc4f84fb386b514575
JD <upkanet@gmail.com>
2021-08-18 17:35:17 +0200

    Web UI Raster

--- src/main.rs
+++ src/main.rs
@@ -54,24 +54,3 @@ fn main() {
         .mount("/", routes![index,electrode,js])
         .launch();
 }
-
-
-/*fn main() -> io::Result<()>
-{
-    println!("RSpiker launch");
-    let mut r = Record::new("data/90003.raw".to_string());
-    let now = Instant::now();
-    r.load();
-    println!("Loading Data - Time elapsed : {}", now.elapsed().as_secs());
-    println!("{}",r.sample_rate);
-    println!("{}",r.adczero);
-    println!("{}",r.el);
-    println!("{}",r.streams);
-    r.filter(200);
-    println!("Filtering - Time elapsed : {}", now.elapsed().as_secs());
-    r.spiker(-4.2);
-    println!("Spikering - Time elapsed : {}", now.elapsed().as_secs());
-    r.raster(2);
-
-    Ok(())
-}*/
