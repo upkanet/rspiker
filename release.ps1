745c13a64116f8a027670c5b79afbf7667aec98b
JD <upkanet@gmail.com>
2021-10-12 16:39:40 +0200

    fix releaser

--- release.ps1
+++ release.ps1
@@ -2,10 +2,12 @@ rm -R release/js
 rm -R release/public
 rm release/rspiker.exe
 rm release/config.json
+rm release/mcstream_wrapper.dll
 $version = Read-Host 'version ?'
 cargo build
 cp -R js release/js
 cp -R public release/public
 cp target/debug/rspiker.exe release/rspiker.exe
 cp config.json release/config.json
+cp mcstream_wrapper.dll release/mcstream_wrapper.dll
 Compress-Archive -Path release\* -DestinationPath release-$version.zip
